6ffa179ad8a17978051aa3ba1a5e789c8b5369d9
Younes <187375804+Younesdjzz@users.noreply.github.com>
2025-07-14 11:59:46 +0200

    Added html/css linked with Flask

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
+from flask import Flask, render_template
 import requests
 
-username = "Younesdjzz"  # Your LeetCode username
-url = "https://leetscan.vercel.app/{}".format(username)
+app = Flask(__name__)
+@app.route('/')
+def index():
+    username = "Younesdjzz"  # Your LeetCode username
+    url = "https://leetscan.vercel.app/{}".format(username)
 
-response = requests.get(url)
+    response = requests.get(url)
 
-if response.status_code == 200 :
-    data = response.json()
+    if response.status_code == 200 :
+        data = response.json()
+
+        profil_info = {"pfp" : data['profile']['userAvatar'],
+                       "username" : data['username'],
+                       "rank" : data['ranking'],
+                       }
     
-    print("Profil LeetCode :")
-    print(f"- Photo de profil        : {data['profile']['userAvatar']}")
-    print(f"- Pseudo                 : {data['username']}")
-    print(f"- Rang (rank tier)       : {data['ranking']}")
-    print(f"- Total résolus          : {data['totalSolved']} / {data['totalQuestions']}")
-    print()
-    
-    print("Détail par difficulté :")
-    print(f"  • Easy    : {data['easySolved']} / {data['totalEasy']}")
-    print(f"  • Medium  : {data['mediumSolved']} / {data['totalMedium']}")
-    print(f"  • Hard    : {data['hardSolved']} / {data['totalHard']}")
-    
-else:
-    print("Erreur", response.status_code, ": Echec de la récupération des données :(")
+        problem_info = {"totalSolved" : data['totalSolved'], "totalQuestions" : data['totalQuestions'],
+                        "easySolved" : data['easySolved'], "totalEasy" : data['totalEasy'],
+                        "mediumSolved" : data['mediumSolved'], "totalMedium" : data['totalMedium'],
+                        "hardSolved" : data['hardSolved'], "totalHard" : data['totalHard']
+                        }
+    else:
+        profil_info = None
+        problem_info = None
+
+    return render_template('index.html', response_status=response.status_code, profil_info=profil_info, problem_info=problem_info)
