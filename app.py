fa25b20e2b872ff33c9cf1aa79b3339d0ea5708e
Younes <187375804+Younesdjzz@users.noreply.github.com>
2025-08-01 17:31:33 +0200

    Implementation of the stack and problem solved's info + minor modification on the progress bar

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ def index():
     def get_profil_info(data) :
         """
         Pre : This function takes data, a json file, as parameter
-        Post : Return profil_info (pfp, username, rank)
-                      problem_info (easy, medium, hard and total solved)
+        Post : Return profil_info (pfp, username, rank) (type : dict)
+                      problem_info (easy, medium, hard and total solved) (type : dict)
+                      stackInfo (url to the programming language icon and problem solved) (type : dict[dict])
                as a tuple.
         """
         profil_info = {"pfp" : data['profile']['userAvatar'],
@@ -21,14 +22,21 @@ def index():
                         "easySolved" : data['easySolved'], "totalEasy" : data['totalEasy'],
                         "mediumSolved" : data['mediumSolved'], "totalMedium" : data['totalMedium'],
                         "hardSolved" : data['hardSolved'], "totalHard" : data['totalHard']}
+        
+        stackInfo = {"Python" : {"icon" : "static/stack/pythonIcon.jpg",
+                                 "solved" : data['totalSolved'] - 5},
+                     "JavaScript" : {"icon" : "static/stack/javascriptIcon.jpg",
+                                     "solved" : data['totalSolved'] - 60}
+                     }
 
-        return profil_info, problem_info
+        return profil_info, problem_info, stackInfo
     
     now = time.time()
 
     if all(k in CACHE for k in ("profil_info", "problem_info", "timestamp")) and now - CACHE["timestamp"] <= 86400 : # update of CACHE after 24h
         profil_info = CACHE["profil_info"]
         problem_info = CACHE["problem_info"]
+        stackInfo = CACHE["stackInfo"]
         response_status_code = 200
 
     else :
@@ -39,13 +47,19 @@ def index():
         
         if response.status_code == 200 :
             data = response.json()
-            profil_info, problem_info = get_profil_info(data)
+            profil_info, problem_info, stackInfo = get_profil_info(data)
             CACHE["profil_info"] = profil_info
             CACHE["problem_info"] = problem_info
+            CACHE["stackInfo"] = stackInfo
             CACHE["timestamp"] = now
     
         else:
             profil_info = None
             problem_info = None
+            stackInfo = None
 
-    return render_template('index.html', response_status=response_status_code, profil_info=profil_info, problem_info=problem_info)
+    return render_template('index.html',
+                            response_status = response_status_code,
+                            profil_info = profil_info,
+                            problem_info = problem_info,
+                            stackInfo = stackInfo)
