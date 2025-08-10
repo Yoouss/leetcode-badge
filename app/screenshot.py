eda8777d67b20ac78bbd19bcbf8346cc5e6429f0
Younes <187375804+Younesdjzz@users.noreply.github.com>
2025-08-10 16:59:52 +0200

    Added the workflow to take screenshots every 24h

--- app/screenshot.py
+++ app/screenshot.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-def capture_badge(url="http://127.0.0.1:5000/", output_file="app/static/badge.png"):
+def capture_badge(url="https://leetcode-badge.onrender.com/", output_file="app/static/badge.png"):
     with sync_playwright() as p:
         browser = p.firefox.launch(headless=True)
         page = browser.new_page()
