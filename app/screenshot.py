0fa7b6f526c56a15cc1af98570cb47e53b5322ef
Younes <187375804+Younesdjzz@users.noreply.github.com>
2025-08-12 14:02:05 +0200

    Added some print to the screenshot.py

--- app/screenshot.py
+++ app/screenshot.py
@@ -21,7 +21,7 @@ def waking_up_the_website(browser, url, request_cooldown=60, loading_time=90) :
             time.sleep(request_cooldown)
     return False
 
-def waiting_for_the_webite_to_load(page, refresh=360, refresh_cooldown=15) :
+def waiting_for_the_website_to_load(page, refresh=360, refresh_cooldown=15) :
     """
         Pre : page is provided by the function capture_badge()
               refresh is the number of time we'll refresh the page (int)
@@ -32,9 +32,16 @@ def waiting_for_the_webite_to_load(page, refresh=360, refresh_cooldown=15) :
     refresh_count = 0
     while not badge and refresh_count < refresh : # We keep trying for max 1h30
         page.reload()
+        print("Loading the website... Next refresh in {} seconds... ({} refresh/{})".format(refresh_cooldown, refresh_count, refresh))
         badge = page.query_selector("div.badge")
         time.sleep(refresh_cooldown)
         refresh_count += 1
+
+    if badge :
+        print("Website successfully loaded !")
+    else :
+        print("Failed to load the website :(")
+
     return badge   
         
 def capture_badge(url="https://leetcode-badge.onrender.com/", output_file="app/static/badge.png") :
@@ -53,11 +60,11 @@ def capture_badge(url="https://leetcode-badge.onrender.com/", output_file="app/s
             page.close()
             page = waking_up_the_website(browser, url)
             if page == False :
-                print("Failed to wake the website up... screenshot failed")
+                print("Failed to wake the website up... screenshot failed :(")
                 browser.close()
                 return
 
-        badge = waiting_for_the_webite_to_load(page)
+        badge = waiting_for_the_website_to_load(page)
 
         if badge :
             if os.path.exists(output_file) :
@@ -65,7 +72,7 @@ def capture_badge(url="https://leetcode-badge.onrender.com/", output_file="app/s
             badge.screenshot(path=output_file)
             print(f"Screenshot saved as {output_file}")
         else:
-            print("Something went wrong... Maybe the API isn't working or the website has some issues :(")
+            print("Something went wrong... if the website loaded, Maybe the API isn't working or the website has some issues :(")
         browser.close()
 
 if __name__ == "__main__" :
